4366a4135960e395df9ebb9d516446c4a6bad1f8
Kyle <22505634@stu.mmu.ac.uk>
2025-07-17 09:39:31 +0100

    Lastest

---
+++ ControlFlowExcersise.py
@@ -0,0 +1,67 @@
+print("\nQ1a\n")
+# Q1a: Print only the first 5 numbers in this list
+x = [2, 5, 4, 87, 34, 2, 1, 31, 103, 99]
+print  (x[:5])
+
+print("\nQ1b\n")
+# Q1b: Now print only the even numbers in this list (the elements that are themselves even)
+x = [2, 5, 4, 87, 34, 2, 1, 31, 103, 99]
+res = [val for val in x if val % 2 == 0] # List Comprehension
+print(res)
+
+print("\nQ1c\n")
+# Q1c: Now only print the even numbers up to the fifth element in the list (e.g. 2, 4, 34)
+x = [2, 5, 4, 87, 34, 2, 1, 31, 103, 99]
+res = [val for val in x if val % 2 == 0]
+print(res[:5])
+
+# -------------------------------------------------------------------------------------- #
+
+print("\nQ2a\n")
+# Q2a: from the list of names, create another list that consists of only the first letters of each first name
+# e.g. ["Alan Turing", "Leonardo Fibonacci"] -> ["A", "L"]
+names = ["Alan Turing", "Leonardo Fibonacci", "Katherine Johnson", "Annie Easley", "Terence Tao"]
+initial = [letter[0] for letter in names]
+print(initial)
+
+print("\nQ2b\n")
+# Q2b: from the list of names, create another list that consists of only the index of the space in the string
+# HINT: use your_string.index("substring")
+names = ["Alan Turing", "Leonardo Fibonacci", "Katherine Johnson", "Annie Easley", "Terence Tao"]
+space_index = [name.index(" ") for name in names]
+print(space_index)
+
+print("\nQ2c\n")
+# Q2c: from the list of names, create another list that consists of the first and last initial of each individual
+names = ["Alan Turing", "Leonardo Fibonacci", "Katherine Johnson", "Annie Easley", "Terence Tao"]
+initials = [name[0] + [name.index(" ") + 1] for name in names]
+print(initials)
+
+# -------------------------------------------------------------------------------------- #
+
+print("\nQ3a\n")
+# Q3a: Here is a list of lists, print only the lists which have no duplicates
+# Hint: This can be easily done by using sets as a set does not contain duplicates
+list_of_lists = [[1,5,7,3,44,4,1],
+                 ["A", "B", "C"],
+                 ["Hi", "Hello", "Ciao", "By", "Goodbye", "Ciao"],
+                 ["one", "Two", "Three", "Four"]]
+
+
+# A3a:
+
+
+# -------------------------------------------------------------------------------------- #
+
+print("\nQ4a\n")
+# Q4a: Using a while loop, ask the user to input a number greater than 100, if they enter anything else,
+# get them to enter again (and repeat until the conditions are satisfied). Finally print the number that
+# they entered
+
+# A4a:
+
+
+print("\nQ4b\n")
+# Q4b: Continue this code and print "prime" if the number is a prime number and "not prime" otherwise
+
+# A4b:
---
+++ OOP.md
@@ -0,0 +1,44 @@
+# OBJECT ORIENTED PROGRAMMING (OOP)
+
+### OOP is a type of programming paradigm that consists of using classes and objects to structre the program
+
+### 4 Principles to define OOP (Encapsualtion, Inheretance, Polymorphism, Abstraction)
+
+![](./images/oopPrinciples.PNG)
+
+### A class is a blueprint for creating objects. It defines the attributes and methods
+- classStudent
+- first_name
+- surname
+- age
+- methods ()
+
+### A object is an instance of a class 
+#### (This is called an instance which abstracts from the class)
+- myStudent = ClassStudent(first_name =)
+- print(myStudent.first_name)
+
+## Abstraction
+#### Abstrction is a concept used in OOP which aims to handle complexity by hiding unecessary details from the user. 
+- This is done through using abstract classes
+
+![](./images/abstraction.PNG)
+
+## Encapsulation
+#### Encapsulation is a concept used in OOP which is essentially bundling of data and methods/ variables like a class
+#### Encapsulating the data, variables and methods
+
+![](./images/encapsulation.PNG)
+
+## Inheritance
+### Inheritance is a concept used in OOP to obtain attributes from the main class to make a sub-class
+- Class Animal:
+- Class Dog(Animal):
+
+![](./images/inheritance.PNG)
+
+## Polymorphism
+### Polymorphism is a concept in OOP which describes the ability for an object to have more than on realisation
+- Use of a function used in multiple instances
+
+![](./images/polymorphism.PNG)
